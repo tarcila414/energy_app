597f7532c63d5952190fb8dba8dd3421c6007710
tarcila414 <tarcila086@gmail.com>
2021-02-22 23:50:37 -0300

    DAY 1
    Project initialization
    Fist component: Experience Bar

---
+++ energy/src/App.tsx
@@ -0,0 +1,14 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+import './components/ExperienceBar'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar/>
+    </div>
+  );
+}
+
+export default App;
